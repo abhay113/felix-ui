dbb30ed3bb10ecaae610f4bb9e9a717539678b97
abhay113 <abhay@cateina.com>
2025-07-12 12:08:49 +0530

    changed endpoints

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -15,7 +15,7 @@ const userName = localStorage.getItem('name');
  const fetchUsers = async () => {
     try {
      
-      const response = await fetch(`http://localhost:3010/api/v1/getUserByName/${userName}`, {
+      const response = await fetch(`http://localhost:3010/api/v1/getUser/${userName}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -999,7 +999,7 @@ const UserManagementSection = () => {
     try {
       setLoading(true);
       setError(null);
-      const response = await fetch(`http://localhost:3010/api/v1/getUser`, {
+      const response = await fetch(`http://localhost:3010/api/v1/getAllUsers`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
